bc5290036c08ffcafbeeb06e1a11d198ff20ca13
張雅蓁 <zhangyazhen@zhangyazhendeMacBook-Pro.local>
2023-06-24 17:01:07 +0800

    initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
+import postcssImport from 'postcss-import'
+import cssnano from 'cssnano'
+import postcssPresetEnv from 'postcss-preset-env'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    postcssImport({}),
+    cssnano({
+      preset: "default",
+    }),
+    postcssPresetEnv({
+      stage: 1,
+    }),
+    tailwindcss({}),
+    autoprefixer({}),
+  ],
+};
